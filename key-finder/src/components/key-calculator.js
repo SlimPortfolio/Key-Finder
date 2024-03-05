6082cf20c56080cbd5b353574af9f92739bc5af1
Steven Lim <stevenylim17@gmail.com>
2024-03-04 20:54:37 -0600

    made adjustments, works with force-fed data

--- key-finder/src/components/key-calculator.js
+++ key-finder/src/components/key-calculator.js
@@ -38,7 +38,7 @@ export default function KeyCalculator() {
     },
   ];
 
-  const keyDictionary = new Map();
+  let keyDictionary = new Map();
   keyDictionary.set("C", 0);
   keyDictionary.set("C#", 1);
   keyDictionary.set("D", 2);
@@ -52,68 +52,82 @@ export default function KeyCalculator() {
   keyDictionary.set("A#", 10);
   keyDictionary.set("B", 11);
 
-  const valueDictionary = new Map();
-  keyDictionary.set(0, "C");
-  keyDictionary.set(1, "C#");
-  keyDictionary.set(2, "D");
-  keyDictionary.set(3, "D#");
-  keyDictionary.set(4, "E");
-  keyDictionary.set(5, "F");
-  keyDictionary.set(6, "F#");
-  keyDictionary.set(7, "G");
-  keyDictionary.set(8, "G#");
-  keyDictionary.set(9, "A");
-  keyDictionary.set(10, "A#");
-  keyDictionary.set(11, "B");
+  let valueDictionary = new Map();
+  valueDictionary.set(0, "C");
+  valueDictionary.set(1, "C#");
+  valueDictionary.set(2, "D");
+  valueDictionary.set(3, "D#");
+  valueDictionary.set(4, "E");
+  valueDictionary.set(5, "F");
+  valueDictionary.set(6, "F#");
+  valueDictionary.set(7, "G");
+  valueDictionary.set(8, "G#");
+  valueDictionary.set(9, "A");
+  valueDictionary.set(10, "A#");
+  valueDictionary.set(11, "B");
 
-  const quantNote = function (note) {
-    const octave = note.charAt(note.length - 1);
-    const value =
+  let quantNote = function (note) {
+    let octave = note.charAt(note.length - 1);
+    let value =
       octave * 12 +
       keyDictionary.get(note.substring(0, note.length - 1).toString());
     return value;
   };
-  const gapNote = function (note1, note2) {
-    const result = quantNote(note1) - quantNote(note2);
+  let gapNote = function (note1, note2) {
+    let result = quantNote(note1) - quantNote(note2);
     // return Math.abs(result);
     return result;
   };
 
-  const originalSong = {
-    originalKey: "C",
-    lowNote: "E4",
-    highNote: "F5",
-  };
-
-  const vocalist1 = {
-    lowNote: "B3",
-    highNote: "E5",
-  };
-
-  const highNoteSong = originalSong.highNote;
-  const lowNoteSong = originalSong.lowNote;
-  const highNoteVocalist = vocalist1.highNote;
-  const lowNoteVocalist = vocalist1.lowNote;
-  const rangeSong = gapNote(highNoteSong, lowNoteSong);
-  const rangeVocalist = gapNote(highNoteVocalist, lowNoteVocalist);
-
-  const isSingable = function (rSong, rVocalist) {
+  let isSingable = function (rSong, rVocalist) {
     if (rSong > rVocalist) {
       return false;
     } else {
       return true;
     }
   };
-
-  const highGap = function (highSong, highVocalist) {
-    const result = quantNote(note1) - quantNote(note2);
+  let highGap = function (highSong, highVocalist) {
+    let result = quantNote(highVocalist) - quantNote(highSong);
+    return result;
   };
+  let originalSong = {
+    originalKey: "A",
+    lowNote: "A3",
+    highNote: "C#5",
+  };
+
+  let vocalist1 = {
+    lowNote: "B3",
+    highNote: "E5",
+  };
+
+  let keySong = originalSong.originalKey;
+  let highNoteSong = originalSong.highNote;
+  let lowNoteSong = originalSong.lowNote;
+  let highNoteVocalist = vocalist1.highNote;
+  let lowNoteVocalist = vocalist1.lowNote;
+  let rangeSong = gapNote(highNoteSong, lowNoteSong);
+  let rangeVocalist = gapNote(highNoteVocalist, lowNoteVocalist);
+  let singable = isSingable(rangeSong, rangeVocalist);
+  let highCalculation = highGap(highNoteSong, highNoteVocalist);
+  console.log(highNoteSong);
+
+  let keyCalculation = function (singable, highCalculation, originalKey) {
+    if (singable == false) {
+      return "Song is Unsingable";
+    }
+    let newKey = keyDictionary.get(originalKey) + highCalculation;
+    console.log("newk is", newKey);
+    newKey = ((newKey % 12) + 12) % 12;
+    return "New Value is Key of " + valueDictionary.get(newKey);
+  };
+
+  console.log(keyCalculation(singable, highCalculation, keySong));
   return (
     <div>
       <h1>Ideal Vocal Key Calculator</h1>
       <p>Range of the Original Song is {rangeSong}</p>
       <p>Range of the Vocalist is {rangeVocalist}</p>
-      <p>Song is Singable: {isSingable(rangeSong, rangeVocalist)}</p>
     </div>
   );
 }
