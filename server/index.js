a4e1517cfbca58658bb29bbfb91afb89fb3e036c
Steven Lim <stevenylim17@gmail.com>
2024-05-06 21:15:38 -0500

    adjusted index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Server is Running Right?");
-});
+// app.use("/", (req, res) => {
+//   res.send("Server is Running Right?");
+// });
 
-app.listen(5000, console.log("Server is Running on Port 5000"));
+// app.listen(5000, console.log("Server is Running on Port 5000"));
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3001, () => console.log("Server ready on port 3001."));
+
+module.exports = app;
