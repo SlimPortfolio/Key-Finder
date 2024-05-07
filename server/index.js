e103d20d5cce0f89ca8e19d56e81364b0f9c2e27
Steven Lim <stevenylim17@gmail.com>
2024-05-06 22:07:44 -0500

    tried resetting server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,7 @@
 const express = require("express");
 const app = express();
+app.use("/", (req, res) => {
+  res.send("server is running");
+});
 
-// app.use("/", (req, res) => {
-//   res.send("Server is Running Right?");
-// });
-
-// app.listen(5000, console.log("Server is Running on Port 5000"));
-
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-app.listen(3001, () => console.log("Server ready on port 3001."));
-
-module.exports = app;
+app.listen(5000, console.log("server is running on 5000"));
